006abf2a5253c19624add056d6c11684f276dbfa
kacper119p <kacper119p@gmail.com>
2023-09-12 22:56:19 +0200

    Initial commit

---
+++ Source/SpaceProject/SpaceProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEPROJECT_API ASpaceProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
