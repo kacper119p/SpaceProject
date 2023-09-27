d423efa07767859b43a900f9bf96341798846d88
kacper119p <kacper119p@gmail.com>
2023-09-27 02:42:38 +0200

    Added Skybox and Space Dust

---
+++ Source/SpaceProject/Private/SpaceLevel/SpaceDust.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright © Kacper Pastuszka 2023 All rights reserved.
+
+#include "SpaceDust.h"
+
+ASpaceDust::ASpaceDust()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("VFX");
+	RootComponent = NiagaraComponent;
+
+	FollowPlayerComponent = CreateDefaultSubobject<UFollowPlayerComponent>("Follow Player");
+}
