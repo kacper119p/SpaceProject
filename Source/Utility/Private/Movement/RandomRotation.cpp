81eb5cdd0d4bafbeec346cbb40b31d9f43c85c3e
kacper119p <kacper119p@gmail.com>
2023-10-01 14:58:47 +0200

    Asteroid Rotation

---
+++ Source/Utility/Private/Movement/RandomRotation.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright © Kacper Pastuszka 2023 All rights reserved.
+
+
+#include "Movement/RandomRotation.h"
+
+
+URandomRotation::URandomRotation()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickGroup = TG_PrePhysics;
+
+	MinRotationSpeed = 0;
+	MaxRotationSpeed = 0;
+}
+
+
+void URandomRotation::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FVector RotationVector
+		= FVector(FMath::RandRange(0.0f, 1.0f),
+		          FMath::RandRange(0.0f, 1.0f),
+		          FMath::RandRange(0.0f, 1.0f));
+
+	RotationVector = RotationVector.GetSafeNormal();
+
+	Rotation = FRotator(FQuat
+		(RotationVector, FMath::DegreesToRadians(FMath::RandRange(MinRotationSpeed, MaxRotationSpeed))));
+}
+
+
+void URandomRotation::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	GetOwner()->AddActorWorldRotation(Rotation * DeltaTime);
+}
---
+++ Source/Utility/Public/Movement/RandomRotation.h
@@ -0,0 +1,33 @@
+﻿// Copyright © Kacper Pastuszka 2023 All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RandomRotation.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UTILITY_API URandomRotation : public UActorComponent
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (ClampMin = "0", ClampMax = "360", UIMin = "0", UIMax = "360"))
+	float MinRotationSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (ClampMin = "0", ClampMax = "360", UIMin = "0", UIMax = "360"))
+	float MaxRotationSpeed;
+
+private:
+	FRotator Rotation;
+
+public:
+	URandomRotation();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
